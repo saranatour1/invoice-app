e946c7e8a239bd866d13e52dadd2a6a7ff40fa52
saranatour1 <saranatourcontact@gmail.com>
2024-04-19 21:05:32 +0300

    first commit

---
+++ apps/web/nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/tailwindcss','@vueuse/nuxt',],
+  devtools: { enabled: true },
+  tailwindcss: {
+    // Options
+    cssPath: '~/assets/css/tailwind.css',
+    viewer: true,
+  }
+})
